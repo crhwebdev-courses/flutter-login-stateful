150722a760a059bcc9437b0e8e73719c4aafdff6
Carl Hain <crhain@gmail.com>
2019-03-19 09:14:13 -0500

    Add comment to LoginScreen widget to clarify use of GlobalKey

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -14,6 +14,7 @@ class LoginScreenState extends State<LoginScreen> {
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Form(
+        // associate formKey with FormState
         key: formKey,
         child: Column(
           children: [
